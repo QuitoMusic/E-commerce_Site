fb8a450bc4aa3a9481760fbb12d240720892fba5
Francisco Gonzalez <f86gonzalez@outlook.com>
2023-07-17 16:53:24 -0400

    Added id's to models on models/index.js

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -1,16 +1,29 @@
-// import models
 const Product = require('./Product');
 const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id', // This will add category_id to the Product model
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id', // This will add category_id to the Product model
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  foreignKey: 'product_id', // This will add product_id to the ProductTag model
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey: 'tag_id', // This will add tag_id to the ProductTag model
+});
 
 module.exports = {
   Product,
@@ -18,3 +31,4 @@ module.exports = {
   Tag,
   ProductTag,
 };
+
